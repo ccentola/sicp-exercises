cb581e1b90e8ddf01bd0dc85fe88e73176d7f8b0
Carl Centola <carl.centola@gmail.com>
2018-10-30 12:33:34 -0400

    Create 1.03.scm
    **Exercise 1.3**  Define a procedure that takes three numbers as arguments and returns the sum of the squares of the two larger numbers.

---
+++ chapter-1/1.03.scm
@@ -0,0 +1,11 @@
+; The sum of the squares of the two larger numbers
+; equals to the difference of 
+; the sum of the squares of the three numbers and
+; the square of the minimum number
+(define (square x) (* x x))
+
+(define (sum-square-two-larger a b c) 
+  (- (+ (square a) 
+        (square b) 
+        (square c))
+     (square (min a b c))))
