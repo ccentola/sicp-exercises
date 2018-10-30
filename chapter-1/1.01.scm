c3b296dca22b22562657476e967eeb619b53186c
Carl Centola <carl.centola@gmail.com>
2018-10-30 12:16:01 -0400

    Update and rename 1.1.scm to 1.01.scm
    updated file naming and false value

--- chapter-1/1.01.scm
+++ chapter-1/1.01.scm
@@ -6,7 +6,7 @@
 (define a 3)                        ;a = 3
 (define b (+ a 1))                  ;b = 4
 (+ a b (* a b))                     ;19
-(= a b)                             ;false
+(= a b)                             ;#f
 (if (and (> b a) (< b (* a b)))
     b
     a)                              ;4
